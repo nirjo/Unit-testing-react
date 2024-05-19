7a3ae63eee2e40260c856c4f8564bd265102aa3c
nirmalraj joseph <nirmalraj.joseph86@gmail.com>
2024-05-19 19:07:51 +0530

    login_test

--- src/_test_/Login.test.js
+++ src/_test_/Login.test.js
@@ -1,1 +1,19 @@
-test()
+/* eslint-disable testing-library/prefer-presence-queries */
+
+import {render,screen} from '@testing-library/react';
+import Login from '../Login';
+
+test('checking component loader',()=>{
+    //steps for test
+    render(<Login />);
+    expect(screen.queryByText(/Login/)).toBeInTheDocument();
+})
+test('checking email & password inputs are empty',()=>{
+    //steps for test
+    render(<Login />);
+    expect(screen.queryByPlaceholderText('Email')).toHaveValue("");
+    expect(screen.queryByPlaceholderText('Password')).toHaveValue("");
+
+
+
+})
